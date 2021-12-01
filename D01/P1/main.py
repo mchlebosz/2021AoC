ac8f264edf22c77ec06f01a6415a3ca34d66faa3
MChlebosz <mateusz.chlebosz@outlook.com>
2021-12-01 10:40:48 +0100

    Day 1 🚢

--- D01/P1/main.py
+++ D01/P1/main.py
@@ -4,10 +4,16 @@ script_location = Path(__file__).absolute().parent
 with open(script_location / 'input.txt', mode='r', encoding='utf-8') as f:
     inpu = list(map(lambda x: list(x[:len(x)-1].split()), f.readlines()))
 
+prev = int(inpu[0][0])
+counter = 0
+for i in range(1, len(inpu)):
+    now = int(inpu[i][0])
+    if now > prev:
+        counter += 1
+    prev = now
 
 result = list()
-result.append(4)
-result = inpu
+result.append([str(counter)])
 
 
 with open(script_location / "output.out", mode='w', encoding='utf-8') as f:
--- D01/P2/main.py
+++ D01/P2/main.py
@@ -1,13 +1,21 @@
-
 from pathlib import Path
 script_location = Path(__file__).absolute().parent
 with open(script_location / 'input.txt', mode='r', encoding='utf-8') as f:
-    inpu = list(map(lambda x: list(x[:len(x)-1].split()), f.readlines()))
+    inpu = list(map(int, f.readlines()))
 
+prev = sum(inpu[0:3])
+counter = 0
+for i in range(1, len(inpu)-2):
+    now = sum(inpu[i:i+3])
+    if now > prev:
+        # print(prev, now)
+        counter += 1
+    prev = now
 
 result = list()
-result.append(4)
-result = inpu
+
+
+result.append([str(counter)])
 
 
 with open(script_location / "output.out", mode='w', encoding='utf-8') as f:
