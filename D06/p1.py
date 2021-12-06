f33ceeec1fe5b79b65f1fa48de66ca03a520a7b3
MChlebosz <mateusz.chlebosz@outlook.com>
2021-12-07 00:44:29 +0100

    Day 6 🐟🐟🐟🐟🐟

--- D06/p1.py
+++ D06/p1.py
@@ -1,16 +1,31 @@
-
 from pathlib import Path
+
+
+def newFishes(fishes):
+    zeroDayFishes = fishes[0]
+    for i in range(0, 8):
+        fishes[i] = fishes[i+1]
+    fishes[6] += zeroDayFishes
+    fishes[8] = zeroDayFishes
+    return fishes
+
+
 script_location = Path(__file__).absolute().parent
-with open(script_location / 'input.txt', mode='r', encoding='utf-8') as f:
-    inpu = list(map(lambda x: list(x[:len(x)-1].split()), f.readlines()))
+with open(script_location / 'test.txt', mode='r', encoding='utf-8') as f:
+    inpu = list(map(lambda x: list(map(int, x.split(","))), f.readlines()))[0]
+
+fishes = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+for i in inpu:
+    fishes[i] += 1
 
 
-result = list()
-result.append(4)
-result = inpu
+for i in range(256):
+    fishes = newFishes(fishes)
+
+print(sum(fishes))
+
+result = sum(fishes)
 
 
 with open(script_location / "output.out", mode='w', encoding='utf-8') as f:
-    for i in result:
-
-        f.write(" ".join(i)+"\n")
+    f.write(str(result))
