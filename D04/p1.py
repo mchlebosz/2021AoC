aca48ad511fdc386c45692699fa200652eda13a9
MChlebosz <mateusz.chlebosz@outlook.com>
2021-12-06 13:32:33 +0100

    Day 4 🦑

---
+++ D04/p1.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+from collections import Counter
+
+
+def splitBingoes(bingoes):
+    newBingoes = list()
+    for i in range(int(len(bingoes)/6)):
+        tempBingo = list()
+        tempBingo.append(bingoes[6*i+1])
+        tempBingo.append(bingoes[6*i+2])
+        tempBingo.append(bingoes[6*i+3])
+        tempBingo.append(bingoes[6*i+4])
+        tempBingo.append(bingoes[6*i+5])
+        newBingoes.append(tempBingo)
+    return newBingoes
+
+
+def replaceElem(bingo, a):
+    newBingo = [["" for _ in range(5)] for _ in range(5)]
+    for i in range(len(bingo)):
+        for j in range(len(bingo[i])):
+            if bingo[i][j] == a:
+                newBingo[i][j] = "B"
+            else:
+                newBingo[i][j] = bingo[i][j]
+    # print(newBingo)
+    return newBingo
+
+
+def checkBingo(bingo):
+    for i in range(5):
+        if bingo[i].count("B") == 5:
+
+            return True
+        if(bingo[0][i] == "B" and bingo[1][i] == "B" and
+                bingo[2][i] == "B" and bingo[3][i] == "B" and bingo[4][i] == "B"):
+
+            return True
+    return False
+
+
+script_location = Path(__file__).absolute().parent
+with open(script_location / 'input.txt', mode='r', encoding='utf-8') as f:
+    numbers = f.readline()[: -1].split(",")
+    bingoes = list(map(lambda x: x[: len(x)-1].split(), f.readlines()))
+
+bingoes = splitBingoes(bingoes)
+
+
+foundBingo = list()
+foundAtNum = -5
+for number in numbers:
+    if foundAtNum >= 0:
+        break
+
+    bingoes = list(map(lambda x: replaceElem(x, number), bingoes))
+    for i in range(len(bingoes)):
+        if checkBingo(bingoes[i]):
+            foundBingo = bingoes[i]
+            foundAtNum = int(number)
+
+#print(bingoes, "\n")
+print(foundAtNum, foundBingo)
+
+sumBingo = 0
+for row in foundBingo:
+    for i in row:
+        if i != "B":
+            sumBingo += int(i)
+
+
+print(foundAtNum*sumBingo)
+
+result = [[str(foundAtNum*sumBingo)]]
+
+with open(script_location / "output.out", mode='w', encoding='utf-8') as f:
+    for i in result:
+
+        f.write(" ".join(i)+"\n")
