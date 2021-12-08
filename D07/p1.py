f5bc3949ced62b94315d53c9c06c669adcb66faf
MChlebosz <mateusz.chlebosz@outlook.com>
2021-12-08 16:40:24 +0100

    Day 7 🐋

--- D07/p1.py
+++ D07/p1.py
@@ -1,13 +1,20 @@
 
 from pathlib import Path
+import statistics
 script_location = Path(__file__).absolute().parent
 with open(script_location / 'input.txt', mode='r', encoding='utf-8') as f:
-    inpu = list(map(lambda x: list(x[:len(x)-1].split()), f.readlines()))
+    inpu = list(map(int, f.readlines()[0].split(",")))
 
+
+inpu = sorted(inpu)
+median = statistics.median(inpu)
+print(inpu, median)
+sumFuel = 0
+for i in inpu:
+    sumFuel += abs(i-int(median))
 
 result = list()
-result.append(4)
-result = inpu
+result.append([str(sumFuel)])
 
 
 with open(script_location / "output.out", mode='w', encoding='utf-8') as f:
--- D07/p2.py
+++ D07/p2.py
@@ -1,13 +1,31 @@
 
 from pathlib import Path
+import statistics
 script_location = Path(__file__).absolute().parent
 with open(script_location / 'input.txt', mode='r', encoding='utf-8') as f:
-    inpu = list(map(lambda x: list(x[:len(x)-1].split()), f.readlines()))
+    inpu = list(map(int, f.readlines()[0].split(",")))
 
+
+inpu = sorted(inpu)
+median = statistics.median(inpu)
+print(inpu[0], inpu[-1], median)
+
+minSumFuel = float("inf")
+for currPos in range(inpu[-1]):
+    print((currPos/inpu[-1])*100, "%")
+    currentSumFuel = 0
+
+    for item in inpu:
+        currentSumFuel += sum(range(1, abs(int(currPos) - int(item))+1))
+        #print(currPos, item, numFuel)
+    # print(currentSumFuel)
+    minSumFuel = min(minSumFuel, currentSumFuel)
+
+
+print(minSumFuel)
 
 result = list()
-result.append(4)
-result = inpu
+result.append([str(minSumFuel)])
 
 
 with open(script_location / "output.out", mode='w', encoding='utf-8') as f:
