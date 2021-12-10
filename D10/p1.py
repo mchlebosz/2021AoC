9f1ef61ab0c652c0a20159141799b6e38e871dd7
MChlebosz <mateusz.chlebosz@outlook.com>
2021-12-10 15:01:42 +0100

    Day 10 ({[<>]})

--- D10/p1.py
+++ D10/p1.py
@@ -1,13 +1,44 @@
-
 from pathlib import Path
+
+
+def cropLine(line):
+    closeBrackets = {"{": "}", "[": "]", "(": ")", "<": ">"}
+    s = []
+    for t in line:
+        s.append(t)
+        while len(s) >= 2 and s[-2] in closeBrackets and s[-1] == closeBrackets[s[-2]]:
+            s.pop()
+
+            s.pop()
+    return s
+
+
+def findCorruptedPair(line):
+    closing = {"}": 1197, "]": 57,  ")": 3,  ">": 25137}
+    for i in range(1, len(line)):
+        if line[i] in closing:
+            return closing[line[i]]
+    return 0
+
+
 script_location = Path(__file__).absolute().parent
 with open(script_location / 'input.txt', mode='r', encoding='utf-8') as f:
-    inpu = list(map(lambda x: list(x[:len(x)-1].split()), f.readlines()))
+    inpu = list(map(lambda x: x[:len(x)-1], f.readlines()))
+
+total = 0
+
+for l in inpu:
+    lista = cropLine(l)
+    # print(lista)
+    if findCorruptedPair(lista) > 0:
+        print(l)
+    total += findCorruptedPair(lista)
+
+print(total)
 
 
 result = list()
-result.append(4)
-result = inpu
+result.append([str(total)])
 
 
 with open(script_location / "output.out", mode='w', encoding='utf-8') as f:
--- D10/p2.py
+++ D10/p2.py
@@ -1,13 +1,60 @@
-
 from pathlib import Path
+
+
+def cropLine(line):
+    closeBrackets = {"{": "}", "[": "]", "(": ")", "<": ">"}
+    s = []
+    for t in line:
+        s.append(t)
+        while len(s) >= 2 and s[-2] in closeBrackets and s[-1] == closeBrackets[s[-2]]:
+            s.pop()
+
+            s.pop()
+    return s
+
+
+def findCorruptedPair(line):
+    closing = {"}": 1197, "]": 57,  ")": 3,  ">": 25137}
+    for i in range(1, len(line)):
+        if line[i] in closing:
+            return True
+    return False
+
+
 script_location = Path(__file__).absolute().parent
 with open(script_location / 'input.txt', mode='r', encoding='utf-8') as f:
-    inpu = list(map(lambda x: list(x[:len(x)-1].split()), f.readlines()))
+    inpu = list(map(lambda x: x[:len(x)-1], f.readlines()))
 
+
+notCorruptedRev = []
+
+for l in inpu:
+    lista = cropLine(l)
+    if not findCorruptedPair(lista):
+        notCorruptedRev.append(list(reversed(lista)))
+
+
+outputs = []
+brackets = {"{": 3, "[": 2, "(": 1, "<": 4}
+
+# print(notCorruptedRev)
+for l in notCorruptedRev:
+    total = 0
+    for i in range(len(l)):
+        total *= 5
+        total += brackets[l[i]]
+
+    # print(total)
+    outputs.append(total)
+
+outputs.sort()
+
+##
+print(outputs[len(outputs) // 2])
 
 result = list()
-result.append(4)
-result = inpu
+
+result.append([str(outputs[len(outputs) // 2])])
 
 
 with open(script_location / "output.out", mode='w', encoding='utf-8') as f:
